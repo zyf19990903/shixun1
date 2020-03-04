d86452acb14851ed1999a1d74d016ff0c7822e54
zyf <2244152547@.qq.com>
2020-03-04 17:13:58 +0800

    add  shoppingCart 的添加修改删除

--- workfiles/Store-front/viewmodels/product-show.js
+++ workfiles/Store-front/viewmodels/product-show.js
@@ -9,17 +9,54 @@ var show = new Vue({
         description: '',
         stockQuantity: '',
         mainPicUrl: '',
-        otherPicUrls: ''
+        otherPicUrls: [],
+        quantity: 1,
+        myShoppingCart: []
+    },
+    computed:{
+      otherPicUrlsJson() {
+        return this.otherPicUrls.toString();
+      }
     },
     mounted(){
+      var myShoppingCartJson = localStorage['myShoppingCartJson'];
+      this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
+
         var url = new URL(location.href);
         this.productId = url.searchParams.get("productId");
         if (!this.productId) {
+          alert('productId is null');
             return;
         }
         this.getProductById();
     },
     methods: {
+      handleAddToCartClick(){
+        var myShoppingCartJson = localStorage['myShoppingCartJson'];
+        this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
+        //遍历购物车里面的商品，一个一个比对，看是否购物车里面的商品与此商品一样，如果全等,添加的话数量会改变
+        var cartProduct = this.myShoppingCart.find(p => p.productId === this.productId);
+        if(cartProduct){
+          console.log('cart product exist');
+          var originQuantity = parseInt(cartProduct.quantity);
+          var addQuantity = parseInt(this.quantity);
+          cartProduct.quantity = originQuantity + addQuantity;
+        }else{
+          //不存在的话就直接添加进这个对象
+          cartProduct = {
+            productId: this.productId,
+            productCode: this.productCode,
+            productName: this.productName,
+            mainPicUrl: this.mainPicUrl,
+            unitPrice: this.price,
+            quantity: this.quantity
+          };
+          this.myShoppingCart.push(cartProduct);
+        }
+        //重新序列化存储在这个对象中
+        localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+        this.$message.success('添加购物车成功')
+      },
         getProductById() {
             axios.get('/product/show', {
                 params: {
---
+++ workfiles/Store-front/viewmodels/shopping-cart.js
@@ -0,0 +1,37 @@
+var shoppingCart = new Vue({
+  el: '#shoppingCart',
+  data: {
+    myShoppingCart:[]
+  },
+  computed:{//用来计算的函数
+    totalPrice(){
+      //遍历购物车里面的商品,计算此商品的总价
+      var subTotalPrices = this.myShoppingCart.map(p =>{
+        return p.unitPrice * p.quantity;
+      })
+      //相当于遍历刚刚计算的每个商品的总价，然后两两相加，直至到0
+      var totalPrice = subTotalPrices.reduce((a,b) => a+b,0);
+      var totalPriceStr = totalPrice.toFixed(2);//四舍五入2位小数
+      totalPrice = parseFloat(totalPriceStr);
+      return totalPrice;
+    }
+  },
+  mounted(){
+      var myShoppingCartJson = localStorage['myShoppingCartJson'];
+      //如果有值则反序列化，没值就是空数组
+      this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson):[];
+  },
+  methods:{
+    handleUpdate(index, row){
+      localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+      this.$message.success('修改购物车成功');
+    },
+    handleDelete(index, row){
+      if(confirm('确定要删除吗')){
+        this.myShoppingCart.splice(index,1);
+        localStorage['myShoppingCartJson']  = JSON.stringify(this.myShoppingCart);
+        this.$message.success('删除购物车成功')
+      }
+    }
+  }
+})
