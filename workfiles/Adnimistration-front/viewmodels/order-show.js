b4daf95671c3dc5da73d550f3247b23d4ca9dd2f
zyf <2244152547@.qq.com>
2020-03-11 17:07:30 +0800

    admin  修改 order 展示页面   时间+价格+状态  美化

--- workfiles/Adnimistration-front/viewmodels/order-show.js
+++ workfiles/Adnimistration-front/viewmodels/order-show.js
@@ -35,7 +35,21 @@ var ordershow = new Vue({
         ],
         createHistoryOrderStatus: '',
         createHistoryCustomerNotified: false,
-        createHistoryComment: ''
+        createHistoryComment: '',
+        shipMethods: [
+          { value: 0, label: 'EMS' },
+          { value: 1, label: '顺丰' },
+          { value: 2, label: '圆通' },
+          { value: 3, label: '中通' },
+          { value: 4, label: '申通' }
+        ],
+        payMethods: [
+          { value: 0, label: '货到付款' },
+          { value: 1, label: '借记卡' },
+          { value: 2, label: '信用卡' },
+          { value: 3, label: '微信支付' },
+          { value: 4, label: '支付宝' }
+        ]
     },
     mounted() {
          var url = new URL(location.href);
