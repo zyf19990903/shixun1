ec1fbead2df133a8c04762584ef308623fc1243f
zyf <2244152547@.qq.com>
2020-03-11 17:24:04 +0800

    store  修改 return新建页面的 退货理由  美化

--- workfiles/Store-front/viewmodels/return-create.js
+++ workfiles/Store-front/viewmodels/return-create.js
@@ -11,8 +11,14 @@ var returnCreate = new Vue({
         productName: '',
         quantity: '',
         reason: '',
+        reasons: [
+          { value: 0, label: '发货过期' },
+          { value: 1, label: '订单错误' },
+          { value: 2, label: '收到错误商品' },
+          { value: 3, label: '质量问题' }
+        ],
         comment: '',
-        opened: '',
+        opened: ''
     },
     computed: {
         orderTimestamp() {
